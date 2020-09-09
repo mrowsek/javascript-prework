5e47a176c012f1b3f35cd7c6e0f6040258d2c6bf
kamil mrowka <kamil.mrowka@outlook.com.au>
2020-09-09 16:35:37 +0200

    add anonymous function

--- js/script.js
+++ js/script.js
@@ -1,10 +1,17 @@
 
+document.getElementById('play-rock').addEventListener('click', function(){playGame('1');});
+document.getElementById('play-paper').addEventListener('click', function(){playGame('2');});
+document.getElementById('play-scissors').addEventListener('click', function(){playGame('3');});
+
 function playGame(playerInput) {
     clearMessages();
-    
-    function getMoveName(argMoveId){
+
+    console.log('choose number: ' + playerInput);
+    const randomNumber = Math.floor(Math.random() * 3 + 1);
+   
+    let getMoveName = function (argMoveId){
         if(argMoveId == 1){
-        return 'kamień';
+            return 'kamień';
         } else if (argMoveId == 2){
             return 'papier';
         } else if (argMoveId == 3){
@@ -12,12 +19,12 @@ function playGame(playerInput) {
         }
         printMessage('Nie znam ruchu o id ' + argMoveId + '.');
         return 'nieznany ruch';
-    }
-
-    function displayResult(argComputerMove, argPlayerMove) {
+    };
+    
+    let displayResult = function (argComputerMove, argPlayerMove) {
         printMessage('Twój ruch to: ' + argPlayerMove);
         printMessage('Moj ruch to: ' + argComputerMove);
-    
+        
         if( argComputerMove == 'kamień' && argPlayerMove == 'papier'){
             printMessage('Ty wygrywasz!');
         }else if(argComputerMove == 'papier' && argPlayerMove == 'papier'){
@@ -39,23 +46,5 @@ function playGame(playerInput) {
         }
     }
 
-
-    let randomNumber = Math.floor(Math.random() * 3 + 1);
-
-    console.log('Wylosowana liczba to: ' + randomNumber);
-
-    let computerMove = getMoveName(randomNumber);
-
-    // let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
-
-    console.log('Gracz wpisał: ' + playerInput);
-
-    let playerMove = getMoveName(playerInput);
-
-    let score = displayResult(computerMove, playerMove);
-
+    displayResult(getMoveName(playerInput), getMoveName(randomNumber));    
 }
-
-document.getElementById('play-rock').addEventListener('click', function(){playGame('1');});
-document.getElementById('play-paper').addEventListener('click', function(){playGame('2');});
-document.getElementById('play-scissors').addEventListener('click', function(){playGame('3');});
