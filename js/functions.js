34791985471675a87225e8ee207f64ed5c1074a7
Kamil Mrowka <kamil.mrowka@outlook.com.au>
2021-04-03 12:23:58 +0200

    add welcome Box

--- js/functions.js
+++ js/functions.js
@@ -1,9 +1,33 @@
-function printMessage(msg){
+const printMessage = (msg) =>{
 	let div = document.createElement('div');
 	div.innerHTML = msg;
 	document.getElementById('messages').appendChild(div).classList.add('score');
 }
 
-function clearMessages(){
+const clearMessages = () => {
 	document.getElementById('messages').innerHTML = '';
+}
+
+let sumHum = 0;
+let sumPc = 0;
+const winnerPoints = 10;
+const printScore = (arg) =>{
+	if(arg>0){
+		sumHum += arg;
+	}else {
+		sumPc+=1;
+	}
+	document.getElementById('scoore').innerHTML = `Twoje punkty: ${sumHum} Alex: ${sumPc}`;
+	
+	if(sumHum == winnerPoints) {
+		alert('Wyrałeś :-)');
+		sumHum = 0;
+		sumPc = 0;
+		document.getElementById('scoore').innerHTML = `Twoje punkty: ${sumHum} Alex: ${sumPc}`;
+	} else if(sumPc ==winnerPoints){
+		alert('Przegrałeś, ale nie przejmuj się zagrajmy jeszcze raz :-)');
+		sumPc = 0;
+		sumHum = 0;
+		document.getElementById('scoore').innerHTML = `Twoje punkty: ${sumHum} Alex: ${sumPc}`;
+	}
 }
--- js/script.js
+++ js/script.js
@@ -1,50 +1,61 @@
 {
-document.getElementById('play-rock').addEventListener('click', function(){playGame('1');});
-document.getElementById('play-paper').addEventListener('click', function(){playGame('2');});
-document.getElementById('play-scissors').addEventListener('click', function(){playGame('3');});
+    document.getElementById('play-rock').addEventListener('click', ()=>{playGame('1');});
+    document.getElementById('play-paper').addEventListener('click', ()=>{playGame('2');});
+    document.getElementById('play-scissors').addEventListener('click', ()=>{playGame('3');});
 
-function playGame(playerInput) {
-    clearMessages();
+    const welcomeBox = () => {
+        let welcomeDiv = document.getElementById('welcomeBox');
+        let btn = document.createElement('BUTTON');
+        btn.innerHTML = 'Next'
+        welcomeDiv.innerHTML = '<h4>Hej! :-) Nazywam się Aleks. Nauczyłem się ostatnio grać i chciałbym z kimś poćwiczyć. Zagrasz ze mną? Aby rozpocząć wystarczy kliknąć jeden z poniższych przycisków. Miłej zabawy :-)</h4>';
+        welcomeDiv.appendChild(btn).addEventListener('click', ()=> {
+            welcomeDiv.innerHTML = '';
+        });
+    }
 
-    const randomNumber = Math.floor(Math.random() * 3 + 1);
-   
-    const getMoveName = function (argMoveId){
-        if(argMoveId == 1){
-            return 'kamień';
-        } else if (argMoveId == 2){
-            return 'papier';
-        } else if (argMoveId == 3){
-            return 'nożyce';
-        }
-        printMessage('Nie znam ruchu o id ' + argMoveId + '.');
-        return 'nieznany ruch';
-    };
+    const playGame = (playerInput) => {
+        clearMessages();
+
+        const randomNumber = Math.floor(Math.random() * 3 + 1);
     
-    const displayResult = function (argComputerMove, argPlayerMove) {
-        printMessage('Ja: ' + argPlayerMove);
-        printMessage('Pc: ' + argComputerMove);
+        const getMoveName = (argMoveId) => {
+            if(argMoveId == 1){
+                return 'kamień';
+            } else if (argMoveId == 2){
+                return 'papier';
+            } else if (argMoveId == 3){
+                return 'nożyce';
+            }
+            printMessage('Nie znam ruchu o id ' + argMoveId + '.');
+            return 'nieznany ruch';
+        };
         
-        if( argComputerMove == 'kamień' && argPlayerMove == 'papier'){
-            printMessage('Ty wygrywasz!');
-        }else if(argComputerMove == 'papier' && argPlayerMove == 'papier'){
-            printMessage('Remis');
-        }else if (argComputerMove == 'nożyce' && argPlayerMove == 'papier'){
-            printMessage('Przegrałeś');
-        } else if(argComputerMove == 'kamień' && argPlayerMove == 'kamień'){
-            printMessage('Remis');
-        }else if (argComputerMove == 'papier' && argPlayerMove == 'kamień'){
-            printMessage('Przegrałeś');
-        }else if (argComputerMove == 'nożyce' && argPlayerMove == 'kamień'){
-            printMessage('Wygrałeś');
-        }else if(argComputerMove == 'kamień' && argPlayerMove == 'nożyce'){
-            printMessage('Przegrałeś');
-        }else if (argComputerMove == 'papier' && argPlayerMove == 'nożyce'){
-            printMessage('Ty wygrałeś');
-        }else if (argComputerMove == 'nożyce' && argPlayerMove == 'nożyce'){
-            printMessage('Remis');
-        }
-    };
+        const displayResult = function (argComputerMove, argPlayerMove) {
+            printMessage('Twój ruch: ' + argPlayerMove);
+            printMessage('Mój ruch: ' + argComputerMove);
+            
+            if( argComputerMove == 'kamień' && argPlayerMove == 'papier'){
+                printMessage('Wygrałeś'); printScore(1);
+            }else if(argComputerMove == 'papier' && argPlayerMove == 'papier'){
+                printMessage('Remis');
+            }else if (argComputerMove == 'nożyce' && argPlayerMove == 'papier'){
+                printMessage('Przegrałeś'); printScore(-1);
+            } else if(argComputerMove == 'kamień' && argPlayerMove == 'kamień'){
+                printMessage('Remis');
+            }else if (argComputerMove == 'papier' && argPlayerMove == 'kamień'){
+                printMessage('Przegrałeś'); printScore(-1);
+            }else if (argComputerMove == 'nożyce' && argPlayerMove == 'kamień'){
+                printMessage('Wygrałeś'); printScore(1);
+            }else if(argComputerMove == 'kamień' && argPlayerMove == 'nożyce'){
+                printMessage('Przegrałeś'); printScore(-1);
+            }else if (argComputerMove == 'papier' && argPlayerMove == 'nożyce'){
+                printMessage('Wygrałeś'); printScore(1);
+            }else if (argComputerMove == 'nożyce' && argPlayerMove == 'nożyce'){
+                printMessage('Remis');
+            }
+        };
 
-    displayResult(getMoveName(randomNumber), getMoveName(playerInput));    
-}
+        displayResult(getMoveName(randomNumber), getMoveName(playerInput));    
+    }
+    welcomeBox();
 }
